2f4367d1c72c6ef4ee557bbb53a7afbeffc018e4
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-07-09 12:35:09 +0700

    added the tethys gif speed changer
    also changed css in some places

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-// this plays the vine boom on the homepage
 function playMusic(){
     var music = new Audio('media/vineboom.mp3');
     music.play();
+}
+
+function speedTethys(){
+    var image = document.getElementById('Tethys');
+
+    if (image.src.match("media/Tethys.gif")) {
+        image.src = "media/FastTethys.gif"
+        document.getElementById('tethysButton').innerHTML = "Slow down"
+    } else {
+        image.src = "media/Tethys.gif"
+        document.getElementById('tethysButton').innerHTML = "Speed up"
     }
+  };
