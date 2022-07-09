17024a5a0f2bc8ebfd786b0afe48414d0eaf26b2
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-07-09 11:23:17 +0700

    started on adding passive income
    currently freezes the site
    
    fixing later

--- pages/clicker/clicker.js
+++ pages/clicker/clicker.js
@@ -1,12 +1,29 @@
 let points = 0;
 let power = 1;
 
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
 function increment() {
     points += power;
     document.getElementById('pointCount').innerHTML = points;
 }
 
-function double() {
+function powUp() {
     power += 1;
     document.getElementById('clickPower').innerHTML = power;
+}
+
+function idleOn() {
+    while (true) {
+        sleep(2000);
+        points += 1;
+    }
+    document.getElementById('idleOn').innerHTML = "True";
 }
