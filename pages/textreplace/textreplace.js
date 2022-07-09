c9417c436597fd55913f6fddb1f535d72373dbf3
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-07-09 13:13:04 +0700

    edit
    changing the text output in TextReplace to a spinning text (currently doesn't work)
    
    also changed classes and stuff

--- pages/textreplace/textreplace.js
+++ pages/textreplace/textreplace.js
@@ -3,8 +3,18 @@ let textInput;
 function update() {
     textInput = document.getElementById('input').value;
     if (textInput == '')  {
-        document.getElementById('output').innerHTML = "Input text above";
+        document.getElementsByClassName('output').innerHTML = "Text";
     } else {
-        document.getElementById('output').innerHTML = textInput;
+        document.getElementsByClassName('output').innerHTML = textInput;
     }
 }
+
+/*
+function spinOn() {
+    if (document.getElementsByClassName('output').className.match(/(?:^|\s)output(?!\S)/)) {
+        document.getElementsByClassName('output').className = 'spinner';
+    } else {
+        document.getElementsByClassName('spinner').className = 'output';
+    }
+}
+*/
