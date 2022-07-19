39fa99cb75042a350756c66c96e446c1be5dc767
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-07-19 16:23:53 +0100

    trying to add a dark mode switch

--- pages/clicker/clicker.js
+++ pages/clicker/clicker.js
@@ -1,6 +1,22 @@
+// Variables
 let points = 0;
 let power = 1;
+var lightStatus = document.getElementById('light');
 
+// Other functions
+function lightswitch() {
+    const dStyle = document.querySelector('style');
+    dStyle.innerHTML = 'body {font-color = white}';
+    dStyle.innerHTML = 'body {background-color = black}';
+    
+    if(lightStatus = "Off") {
+        document.getElementById('light').innerHTML = "On";
+    } else {
+        document.getElementById('light').innerHTML = "Off"
+    }
+}
+
+// Shop functions
 setInterval(() => {
     document.getElementById('pointCount').innerHTML = points;
     if (points <= 49) {
@@ -21,7 +37,7 @@ function powUp() {
 
 function idleOn() {
     idlePoints();
-    setInterval(idlePoints, 2000);
+    setInterval(idlePoints, 1000);
     document.getElementById('idleOn').innerHTML = "True"
     document.getElementById('passive').disabled = true;
 }
