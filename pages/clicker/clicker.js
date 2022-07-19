2fee71975098ac1e5c1588d21fa750f436a15d08
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-07-19 17:09:17 +0100

    added shop costs

--- pages/clicker/clicker.js
+++ pages/clicker/clicker.js
@@ -1,17 +1,33 @@
 // Variables
 let points = 0;
 let power = 1;
+let powerCost;
+let idleInterval;
 var lightStatus = document.getElementById('light');
 
 // Other functions
-function lightswitch() {
-    const dStyle = document.querySelector('style');
-    dStyle.innerHTML = 'body {font-color = white}';
-    dStyle.innerHTML = 'body {background-color = black}';
+function check() {
+    powerCost = 20 * power
     
-    if(lightStatus = "Off") {
-        document.getElementById('light').innerHTML = "On";
+    if (points < powerCost) {
+        document.getElementById('double').disabled = true;
     } else {
+        document.getElementById('double').disabled = false;
+    }
+
+    if (points <= 49) {
+        document.getElementById('passive').disabled = true;
+    } else {
+    document.getElementById('passive').disabled = false;
+    }
+}
+
+setInterval(check, 10);
+
+function lightswitch() {
+    if(lightStatus.innerHTML == "Off") {
+        document.getElementById('light').innerHTML = "On";
+    } else if (lightStatus.innerHTML == "On") {
         document.getElementById('light').innerHTML = "Off"
     }
 }
@@ -19,11 +35,6 @@ function lightswitch() {
 // Shop functions
 setInterval(() => {
     document.getElementById('pointCount').innerHTML = points;
-    if (points <= 49) {
-        document.getElementById('passive').disabled = true
-    } else {
-    document.getElementById('passive').disabled = false
-    }
 }, 50)
 
 function increment() {
@@ -32,12 +43,15 @@ function increment() {
 
 function powUp() {
     power++;
+    points -= powerCost;
     document.getElementById('clickPower').innerHTML = power;
 }
 
 function idleOn() {
+    idleInterval = 1000;
     idlePoints();
-    setInterval(idlePoints, 1000);
+    points -= 50;
+    setInterval(idlePoints, idleInterval);
     document.getElementById('idleOn').innerHTML = "True"
     document.getElementById('passive').disabled = true;
 }
