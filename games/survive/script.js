dc314411b3504307847eb3526571cc6f4a547089
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-02-09 22:08:14 +0000

    wip

---
+++ games/survive/script.js
@@ -0,0 +1,11 @@
+//// Variables
+
+/// Game variables
+let hunger = 20;
+let food = 100;
+
+/// HTML Variables
+var lightStatus = document.getElementById('light');
+var autoStatus = document.getElementById('autoCheck');
+
+///// Functions
