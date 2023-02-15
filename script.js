ef9bc31efb718f182bf273cf67fe5190d0aec162
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-02-15 17:22:26 +0000

    Update script.js
    format

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function playMusic(){
+function playMusic() {
     var music = new Audio('media/vineboom.mp3');
     music.play();
 }
 
-function speedTethys(){
+function speedTethys() {
     var image = document.getElementById('Tethys');
-    
+
     if (image.src.match("media/Tethys.gif")) {
         image.src = "media/FastTethys.gif"
         document.getElementById('tethysButton').innerHTML = "Slow down"
@@ -15,9 +15,9 @@ function speedTethys(){
     }
 }
 
-function timer(){
+function timer() {
     var sec = 3;
-    var secCount = setInterval(function(){
+    var secCount = setInterval(function () {
         sec--;
         if (sec < 0) {
             document.getElementById('timer').innerHTML = "0";
