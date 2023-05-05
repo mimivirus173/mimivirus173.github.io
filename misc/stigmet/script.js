94132716086ef4b68a6a6eef06583dfdebe400da
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-05-05 15:33:38 +0100

    stigmet
    converted the ting i made in python
    fyri nú er tað bara alisfrøði

---
+++ misc/stigmet/script.js
@@ -0,0 +1,51 @@
+// Variablar
+var test = 1;
+
+var maxStig;
+var tíniStig;
+
+var initMet;
+var finalMet;
+
+// Funktiónin til at rokna karakter
+function calc() {
+    // Finn virðini
+    maxStig = parseInt(document.getElementById('max').value);
+    tíniStig = parseInt(document.getElementById('stig').value);
+
+    // Rokna prosent
+    initMet = parseInt((tíniStig / maxStig) * 100);
+
+    // Rokna karakter
+    switch (true) {
+        case initMet <= 6:
+            finalMet = -3;
+            break;
+        case initMet <= 33:
+            finalMet = 00;
+            break;
+        case initMet <= 39:
+            finalMet = 02;
+            break;
+        case initMet <= 55:
+            finalMet = 4;
+            break;
+        case initMet <= 75:
+            finalMet = 7;
+            break;
+        case initMet <= 92:
+            finalMet = 10;
+            break;
+        case initMet <= 100:
+            finalMet = 12;
+            break;
+        default:
+            finalMet = "?";
+    }
+
+    // Prenta á síðuna
+    document.getElementById('met').innerHTML = finalMet;
+}
+
+// Loopa funktiónina
+setInterval(calc, 10);
