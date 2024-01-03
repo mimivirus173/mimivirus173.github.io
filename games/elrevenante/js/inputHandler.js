671c6657e07acb86eec96b32a4ead10fd65137fe
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-01-03 09:11:28 +0000

    commetn

--- games/elrevenante/js/inputHandler.js
+++ games/elrevenante/js/inputHandler.js
@@ -1,3 +1,4 @@
+// When key is down
 addEventListener("keydown", function(event) {
     if(event.code == 'KeyW') keyUp = 1;
     if(event.code == 'KeyS') keyDown = 1;
@@ -6,6 +7,7 @@ addEventListener("keydown", function(event) {
     if(event.code == 'KeyD') keyRight = 1;
 })
 
+// When key is up
 addEventListener("keyup", function(event) {
     if(event.code == 'KeyW') keyUp = 0;
     if(event.code == 'KeyS') keyDown = 0;
--- games/elrevenante/js/main.js
+++ games/elrevenante/js/main.js
@@ -1,17 +1,20 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("game");     // Find the game canvas
+const ctx = canvas.getContext("2d");                // Dfine canvas as 2d
 
+// Movement variables
 let x = 0;
 let y = 0;
 
 let xvel = 0;
 let yvel = 0;
 
+// Key press registerers
 let keyUp = 0;
 let keyDown = 0;
 let keyLeft = 0;
 let keyRight = 0;
 
+// Smooth movement
 function movementPhys(speed, friction) {
     xvel += (keyRight + (0 - keyLeft)) * speed;
     xvel = xvel * friction;
@@ -20,6 +23,7 @@ function movementPhys(speed, friction) {
     yvel = yvel * friction;
 };
 
+// Update the canvas
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     movementPhys(2, 0.9);
