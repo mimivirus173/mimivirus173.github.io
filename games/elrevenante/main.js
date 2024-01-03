1a263ebb7a7afdcedfd841e7b3ecdb300b5052a7
mimivirus173 <hap0412@outlook.com>
2024-01-03 18:47:41 +0000

    background tiled

--- games/elrevenante/main.js
+++ games/elrevenante/main.js
@@ -1,5 +1,3 @@
-// main.js
-
 // Set up canvas
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -7,30 +5,42 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-doomguy.init();
+// Load background image
+let bgImg = new Image();
+bgImg.src = 'media/156.png';
 
-// Update canvas
-function update() {
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+bgImg.onload = function() {
+    // Initialize actors
+    doomguy.init();
+
+    // Update canvas
+    function update() {
+        // Clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // Draw background
+        let pattern = ctx.createPattern(bgImg, 'repeat');   // Create pattern
+        ctx.fillStyle = pattern;                            // Set fill style to pattern
+        ctx.fillRect(0, 0, canvas.width, canvas.height);    // Fill canvas
     
-    doomguy.update();
+        doomguy.update();
 
-    /// Draw actors
+        /// Draw actors
 
-    // Flip doomguy if he is past the middle of the canvas and is shooting
-    if (doomguy.x > canvas.width / 2 && doomguy.keySpace) {
-        ctx.save(); // Save the current context
-        ctx.scale(-1, 1); // Flip context horizontally
-        ctx.drawImage(player, -doomguy.x - player.width, doomguy.y, 41, 56); // Draw the image
-        ctx.restore(); // Restore the context to its original state
-    } else {
-        ctx.drawImage(player, doomguy.x, doomguy.y, 41, 56);
-    }
+        // Flip doomguy if he is past the middle of the canvas and is shooting
+        if (doomguy.x > canvas.width / 2 && doomguy.keySpace) {
+            ctx.save(); // Save the current context
+            ctx.scale(-1, 1); // Flip context horizontally
+            ctx.drawImage(player, -doomguy.x - player.width, doomguy.y, 41, 56); // Draw the image
+            ctx.restore(); // Restore the context to its original state
+        } else {
+            ctx.drawImage(player, doomguy.x, doomguy.y, 41, 56);
+        }
     
-    ctx.drawImage(revenant, window.innerWidth / 2, window.innerHeight / 2, 52, 101);
+        ctx.drawImage(revenant, window.innerWidth / 2, window.innerHeight / 2, 52, 101);
 
-    requestAnimationFrame(update);
-};
+        requestAnimationFrame(update);
+    };
 
-update();
+    update();
+}
