e8876286889aa400f94115a48d8b55a001d9e1e4
mimivirus173 <hap0412@outlook.com>
2024-01-03 16:55:20 +0000

    merge inputhandler

--- games/elrevenante/js/main.js
+++ games/elrevenante/js/main.js
@@ -24,6 +24,24 @@ let keyDown = 0;
 let keyLeft = 0;
 let keyRight = 0;
 
+// When key is down
+addEventListener("keydown", function(event) {
+    if(event.code == 'KeyW') keyUp = 1;
+    if(event.code == 'KeyS') keyDown = 1;
+    
+    if(event.code == 'KeyA') keyLeft = 1;
+    if(event.code == 'KeyD') keyRight = 1;
+})
+
+// When key is up
+addEventListener("keyup", function(event) {
+    if(event.code == 'KeyW') keyUp = 0;
+    if(event.code == 'KeyS') keyDown = 0;
+    
+    if(event.code == 'KeyA') keyLeft = 0;
+    if(event.code == 'KeyD') keyRight = 0;
+})
+
 // Smooth movement
 function movementPhys(speed, friction) {
     xvel += (keyRight + (0 - keyLeft)) * speed;
