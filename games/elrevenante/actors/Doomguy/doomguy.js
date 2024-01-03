d8c7d599fd114fd74a218a11be9aca9951d2eaf5
mimivirus173 <hap0412@outlook.com>
2024-01-03 17:38:20 +0000

    moved doomguy into another file
    thank you gpt

--- games/elrevenante/actors/Doomguy/doomguy.js
+++ games/elrevenante/actors/Doomguy/doomguy.js
@@ -1,1 +1,68 @@
 // doomguy.js
+
+var player = new Image();
+player.src = 'actors/Doomguy/media/doomguy.png';
+
+let doomguy = {
+    x: 0,
+    y: 0,
+    xvel: 0,
+    yvel: 0,
+    keyUp: 0,
+    keyDown: 0,
+    keyLeft: 0,
+    keyRight: 0,
+
+    init: function() {
+        addEventListener("keydown", this.keyDownHandler);
+        addEventListener("keyup", this.keyUpHandler);
+    },
+
+    keyDownHandler: function(event) {
+        switch (event.code) {
+            case 'KeyW':
+                doomguy.keyUp = 1;
+                break;
+            case 'KeyS':
+                doomguy.keyDown = 1;
+                break;
+            case 'KeyA':
+                doomguy.keyLeft = 1;
+                break;
+            case 'KeyD':
+                doomguy.keyRight = 1;
+                break;
+        }
+    },
+
+    keyUpHandler: function(event) {
+        switch (event.code) {
+            case 'KeyW':
+                doomguy.keyUp = 0;
+                break;
+            case 'KeyS':
+                doomguy.keyDown = 0;
+                break;
+            case 'KeyA':
+                doomguy.keyLeft = 0;
+                break;
+            case 'KeyD':
+                doomguy.keyRight = 0;
+                break;
+        }
+    },
+
+    movementPhys: function(speed, friction) {
+        doomguy.xvel += (doomguy.keyRight + (0 - doomguy.keyLeft)) * speed;
+        doomguy.xvel = doomguy.xvel * friction;
+
+        doomguy.yvel += (doomguy.keyDown + (0 - doomguy.keyUp)) * speed;
+        doomguy.yvel = doomguy.yvel * friction;
+    },
+
+    update: function() {
+        doomguy.movementPhys(2, 0.9);
+        doomguy.x += doomguy.xvel;
+        doomguy.y += doomguy.yvel;
+    }
+};
--- games/elrevenante/main.js
+++ games/elrevenante/main.js
@@ -1,74 +1,25 @@
 // main.js
 
-const canvas = document.getElementById("game");     // Find the game canvas
-const ctx = canvas.getContext("2d");                // Define canvas as 2d
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
-// Tweak canvas size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Actors
-var player = new Image();
-player.src = 'actors/Doomguy/media/doomguy.png'
 var revenant = new Image();
-revenant.src = 'actors/Revenant/media/revenant1.png'
+revenant.src = 'actors/Revenant/media/revenant1.png';
 
-// Movement variables
-let x = 0;
-let y = 0;
+doomguy.init();
 
-let xvel = 0;
-let yvel = 0;
-
-// Key press registerers
-let keyUp = 0;
-let keyDown = 0;
-let keyLeft = 0;
-let keyRight = 0;
-
-// When key is down
-addEventListener("keydown", function(event) {
-    if(event.code == 'KeyW') keyUp = 1;
-    if(event.code == 'KeyS') keyDown = 1;
-    
-    if(event.code == 'KeyA') keyLeft = 1;
-    if(event.code == 'KeyD') keyRight = 1;
-})
-
-// When key is up
-addEventListener("keyup", function(event) {
-    if(event.code == 'KeyW') keyUp = 0;
-    if(event.code == 'KeyS') keyDown = 0;
-    
-    if(event.code == 'KeyA') keyLeft = 0;
-    if(event.code == 'KeyD') keyRight = 0;
-})
-
-// Smooth movement
-function movementPhys(speed, friction) {
-    xvel += (keyRight + (0 - keyLeft)) * speed;
-    xvel = xvel * friction;
-
-    yvel += (keyDown + (0 - keyUp)) * speed;
-    yvel = yvel * friction;
-};
-
-// Update the canvas
 function update() {
-    // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    // Doomguy movement
-    movementPhys(2, 0.9);
-    x += xvel;
-    y += yvel;
+    doomguy.update();
 
-    // Draw actors
-    ctx.drawImage(player, x, y, 41, 56);
+    ctx.drawImage(player, doomguy.x, doomguy.y, 41, 56);
     ctx.drawImage(revenant, window.innerWidth / 2, window.innerHeight / 2, 52, 101);
 
     requestAnimationFrame(update);
-    // console.log(keyUp, keyDown, keyLeft, keyRight);
 };
 
 update();
