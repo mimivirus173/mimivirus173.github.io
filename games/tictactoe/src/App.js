186c8b9df2c2a4333a2ebc711770a35ebc1b9208
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-01-17 08:56:54 +0000

    ract

--- games/tictactoe/src/App.js
+++ games/tictactoe/src/App.js
@@ -1,3 +1,48 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from 'react';
+
+// Board display
+export default function Board() {
+  // Array counting squares
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  // Board display
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
+
+// Squares in the board
+function Square() {
+  const [value, setValue] = useState(null);
+  
+  // When clicking square
+  function handleClick() {
+    setValue('X');
+  }
+
+  // Button display
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
