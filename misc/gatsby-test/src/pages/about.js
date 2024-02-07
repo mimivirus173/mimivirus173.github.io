5c367a7ce5ca81382d0b8d29bc70beb7417dec12
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-02-07 10:59:40 +0000

    Create about.js

---
+++ misc/gatsby-test/src/pages/about.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
