2f4221b8091f3b0592c2ec4116888facdef6c56d
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-02-07 11:17:32 +0000

    2

--- misc/gatsby-test/src/pages/about.js
+++ misc/gatsby-test/src/pages/about.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
     <main>
-      <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p>Hi there! I'm </p>
+      <Layout pageTitle="About Me">
+        <p>I am the</p>
+      </Layout>
     </main>
   )
 }
--- misc/gatsby-test/src/pages/index.js
+++ misc/gatsby-test/src/pages/index.js
@@ -1,14 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Layout pageTitle="Home Page"> 
+        <p>I'm making this</p>
+      </Layout>
     </main>
   )
 }
