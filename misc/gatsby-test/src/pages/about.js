33c116cbae9012e335317825ec88e7584c57a2ca
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-02-07 11:07:19 +0000

    Update about.js

--- misc/gatsby-test/src/pages/about.js
+++ misc/gatsby-test/src/pages/about.js
@@ -1,19 +1,16 @@
-// Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-// Step 2: Define your component
 const AboutPage = () => {
   return (
     <main>
       <h1>About Me</h1>
-      <Link to="/about">About</Link>
-      <p>Hi there! I'm</p>
+      <Link to="/">Back to Home</Link>
+      <p>Hi there! I'm </p>
     </main>
   )
 }
 
 export const Head = () => <title>About Me</title>
 
-// Step 3: Export your component
 export default AboutPage
