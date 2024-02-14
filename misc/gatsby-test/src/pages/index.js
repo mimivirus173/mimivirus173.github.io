ee060e6fb78e34aff6c7c7f9b64fbf9113254089
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2024-02-14 09:25:45 +0000

    man

--- misc/gatsby-test/src/pages/index.js
+++ misc/gatsby-test/src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -8,6 +9,9 @@ const IndexPage = () => {
     <main>
       <Layout pageTitle="Home Page"> 
         <p>I'm making this</p>
+        <StaticImage
+        alt="tethys"
+        src="https://mimivirus173.github.io/media/Tethys.gif"/>
       </Layout>
     </main>
   )
